ca86e1576f1279bea1826aa125ea3acd1b79ebbd
prabhjot-singh11 <85445585+prabhjot-singh11@users.noreply.github.com>
2022-03-04 02:36:36 -0500

    Add files via upload

---
+++ tempartureConverter.js
@@ -0,0 +1,9 @@
+// convert the temparture kelvin to fahrenheit and celsius
+let kelvin = 0
+let Celsius = kelvin-273
+Math.floor(Celsius)
+let Fahrenheit 
+Math.floor(Fahrenheit)
+Fahrenheit = Celsius * (9/5) + 32
+console.log(`temparture in fahrenheit:${Fahrenheit}`)
+console.log(`Temparture in celsius:${Celsius}`)
